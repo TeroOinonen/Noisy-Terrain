6fc6beba80fcad993d0adc0281b31958c70b51fe
Tero Oinonen <dteoi001@edu.xamk.fi>
2023-02-24 13:39:48 +0200

    TankPlacer added

---
+++ Assets/Scripts/PlaceTank.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceTank : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        Vector3 lookfrom = transform.position;
+        Vector3 direction = transform.forward;
+
+        Ray ray = new Ray(lookfrom, direction);
+
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+			DrawVector(lookfrom, hit.point, Color.magenta);
+
+			DrawVector(hit.point, hit.point + hit.normal, Color.green);
+
+			Vector3 vRight = Vector3.Cross(hit.normal,direction);
+
+			DrawVector(hit.point, hit.point + vRight, Color.red);
+
+			Vector3 vForward = Vector3.Cross(vRight, hit.normal);
+
+			DrawVector(hit.point, hit.point + vForward, Color.blue);
+		}
+    }
+
+	private void DrawVector(Vector3 from, Vector3 to, Color selectColor)
+	{
+		Color curr = Gizmos.color;
+		Gizmos.color = selectColor;
+		Gizmos.DrawLine(from, to);
+		// Compute a location from "to towards from with 30degs"
+		Vector3 loc = -(to - from);
+		loc = Vector3.ClampMagnitude(loc, 0.1f);
+		Quaternion rot30 = Quaternion.Euler(0, 0, 30);
+		Vector3 loc1 = rot30 * loc;
+		rot30 = Quaternion.Euler(0, 0, -30);
+		Vector3 loc2 = rot30 * loc;
+		Gizmos.DrawLine(to, to + loc1);
+		Gizmos.DrawLine(to, to + loc2);
+		Gizmos.color = curr;
+	}
+
+
+	private void DrawVectorDir(Vector3 from, Vector3 dir, Color selectColor)
+	{
+		Vector3 to = from + dir;
+		Color curr = Gizmos.color;
+		Gizmos.color = selectColor;
+		Gizmos.DrawLine(from, to);
+		// Compute a location from "to towards from with 30degs"
+		Vector3 loc = -(to - from);
+		loc = Vector3.ClampMagnitude(loc, 0.1f);
+		Quaternion rot30 = Quaternion.Euler(0, 0, 30);
+		Vector3 loc1 = rot30 * loc;
+		rot30 = Quaternion.Euler(0, 0, -30);
+		Vector3 loc2 = rot30 * loc;
+		Gizmos.DrawLine(to, to + loc1);
+		Gizmos.DrawLine(to, to + loc2);
+		Gizmos.color = curr;
+	}
+}
